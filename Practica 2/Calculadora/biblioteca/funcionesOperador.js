0f790b5f8f3f4e49daf71e029bda312cbcec68c7
gh-asc99 <pf.alexsc99@gmail.com>
2025-09-18 11:17:56 +0200

    practica 2 DWEC terminada

--- Practica 2/Calculadora/biblioteca/funcionesOperador.js
+++ Practica 2/Calculadora/biblioteca/funcionesOperador.js
@@ -3,7 +3,6 @@ import { esEntero } from "../../comprobaciones.js";
 
 function operacion(numero1, numero2, operador){
     if(esEntero(numero1)&&esEntero(numero2)){
-        if(operador==="+"||operador==="-"||operador==="*"||operador==="/"||operador==="%"){
             switch(operador){
                 case "+":
                     let resultado = numero1 + numero2;
@@ -19,22 +18,24 @@ function operacion(numero1, numero2, operador){
                     break;
                 case "/":
                     if (numero2===0){
-                        return `No es posible dividir entre 0.`;
+                        return `${numero1} ${operador} ${numero2} = No es posible dividir entre 0.`;
                         break;
                     }else{
                         let resultado3 = numero1 / numero2;
-                        return `${numero1} ${operador} ${numero2} = ${resultad3}`;
+                        return `${numero1} ${operador} ${numero2} = ${resultado3}`;
                         break;
                     }
                 case "%":
                     let resultado4 = numero1 % numero2;
                     return `${numero1} ${operador} ${numero2} = ${resultado4}`;
                     break;
+                default:
+                    return `Para usar la función, el operador deben ser uno entre (+ - * / %). Has introducido: ${operador}`;
             }
-        }else {
-            return `Para usar la función, el operador deben ser uno entre (+ - * / %). Has introducido: ${operador}`;
-        }
+        
     }else {
         return `Para usar la función, los operandos deben ser valores enteros de tipo numérico. Has introducido: ${numero1} y ${numero2}`;
     }
 }
+
+export { operacion };
---
+++ Practica 2/Calculadora/main.js
@@ -0,0 +1,10 @@
+"use strict";
+import { operacion } from "./biblioteca/funcionesOperador.js";
+
+console.log(operacion(5,7,"+"));
+console.log(operacion(9,15,"-"));
+console.log(operacion(8,4,"*"));
+console.log(operacion(10,2,"/"));
+console.log(operacion(6,0,"/"));
+console.log(operacion(9,4,"%"));
+console.log(operacion(5,7,"asc"));
