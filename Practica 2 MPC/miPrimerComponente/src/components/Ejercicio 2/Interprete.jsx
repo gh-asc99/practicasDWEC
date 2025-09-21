a714e12661ee00f97ac6cd48242f505a5b61798c
gh-asc99 <pf.alexsc99@gmail.com>
2025-09-21 14:06:57 +0200

    Practica 2 MPC terminada

--- Practica 2 MPC/miPrimerComponente/src/components/Ejercicio 2/Interprete.jsx
+++ Practica 2 MPC/miPrimerComponente/src/components/Ejercicio 2/Interprete.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import "./Interprete.css";
 
 const Interprete = (props) => {
+    let claseExtra = "";
+  if (props.tipo === "pelicula") claseExtra = "peliculaInterprete_alert";
+  if (props.tipo === "elenco") claseExtra = "peliculaElenco_alert";
     return(
         <>
-        <div className='interprete_alerta'>
+        <div className={`interprete_alert ${claseExtra}`}>
             <div className='imagen'><img src={props.foto} alt="Foto"></img></div>
             <h2>{props.nombre}</h2>
             <p>{props.children}</p>
         </div>
         </>
+        
     );
 };
 
