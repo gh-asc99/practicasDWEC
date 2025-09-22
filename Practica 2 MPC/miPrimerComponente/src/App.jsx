635c037c57f5d15af3f50b44b90b1f8eb30332be
Alejandro <alexsc199x@gmail.com>
2025-09-22 18:17:15 +0200

    inicio prácticas evaluables

--- Practica 2 MPC/miPrimerComponente/src/App.jsx
+++ Practica 2 MPC/miPrimerComponente/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       titulo="Los piratas del Caribe 2" director="Gore Verbinski" portada="https://www.mubis.es/media/movies/966/66037/piratas-del-caribe-2-el-cofre-del-hombre-muerto-original.jpg"
       actor1="Johnny Depp" fotoActor1="https://m.media-amazon.com/images/M/MV5BZjA3NzZiZDktZjc2My00MzY2LThhOWMtZGFjYzg4ZDI2ZWVmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" descripcionActor1="En la piel del Capitán Jack Sparrow."
       actor2="Orlando Bloom" fotoActor2="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Orlando_Bloom_Cannes_2013.jpg/250px-Orlando_Bloom_Cannes_2013.jpg" descripcionActor2="En la piel del joven bucanero William Turner."
-      actor3="Keira Knightley" fotoActor3="https://es.web.img3.acsta.net/c_310_420/medias/nmedia/18/35/37/27/19587090.jpg" descripcionActor3="En la piel de Elizabeth Swann, la hija del gobernador.">
+      actor3="Keira Knightley" fotoActor3="https://www.lavanguardia.com/peliculas-series/images/all/profile/1985/3/116/w1280/pxzcVzTyJBKwfuyRLDtax1Jmr7o.jpg" descripcionActor3="En la piel de Elizabeth Swann, la hija del gobernador.">
         Jack Sparrow descubre que le debe su alma a Davy Jones y busca el legendario cofre que guarda su corazón.
         Will Turner y Elizabeth Swann se ven arrastrados a la peligrosa aventura.
         Traiciones, monstruos marinos y batallas navales marcan la carrera por sobrevivir y dominar los mares.
