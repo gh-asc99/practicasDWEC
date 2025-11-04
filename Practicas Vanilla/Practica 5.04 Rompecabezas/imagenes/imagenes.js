0769a7712259a878977f318a5f98597e5727e77a
Alejandro <alexsc199x@gmail.com>
2025-11-04 16:58:29 +0100

    rompecabezas a mitad

--- Practicas Vanilla/Practica 5.04 Rompecabezas/imagenes/imagenes.js
+++ Practicas Vanilla/Practica 5.04 Rompecabezas/imagenes/imagenes.js
@@ -1,13 +1,13 @@
 "use strict";
 let imagenes = [];
-imagenes.push('.arribaIzq.png');
-imagenes.push('.arribaCent.png');
-imagenes.push('.arribaDer.png');
-imagenes.push('.medioIzq.png');
-imagenes.push('.medioCent.png');
-imagenes.push('.medioDer.png');
-imagenes.push('.bajoIzq.png');
-imagenes.push('.bajoCent.png');
-imagenes.push('.bajoDer.png');
+imagenes.push('/arribaIzq.png');
+imagenes.push('/arribaCent.png');
+imagenes.push('/arribaDer.png');
+imagenes.push('/medioIzq.png');
+imagenes.push('/medioCent.png');
+imagenes.push('/medioDer.png');
+imagenes.push('/bajoIzq.png');
+imagenes.push('/bajoCent.png');
+imagenes.push('/bajoDer.png');
 
 export default imagenes;
--- Practicas Vanilla/Practica 5.04 Rompecabezas/main.js
+++ Practicas Vanilla/Practica 5.04 Rompecabezas/main.js
@@ -1,5 +1,12 @@
 "use strict";
 
-import imprimirImagenesAleatorio from "./biblioteca/imprimirImagenesAleatorio.js";
+import desordenarArray from "./biblioteca/desordenarArray.js";
+import imagenes from "./imagenes/imagenes.js";
 
-imprimirImagenesAleatorio();
+let imagenesDesordenadas = desordenarArray(imagenes);
+
+for (let i = 0; i < imagenesDesordenadas.length; i++){
+    let selectorImagen = document.createElement('img');
+    selectorImagen.setAttribute("src", `./imagenes${imagenesDesordenadas[i]}`);
+    document.getElementById('expositor_imagenes').appendChild(selectorImagen);
+};
