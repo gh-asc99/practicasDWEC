3046ed1ab2b2e4ca9ed4ea3ddbe0ade43d5403da
gh-asc99 <pf.alexsc99@gmail.com>
2026-01-22 14:42:00 +0100

    creando conexion login con supabase

--- Lista Compra Supabase/lista-compra-supabase/src/context/ProveedorSesion.jsx
+++ Lista Compra Supabase/lista-compra-supabase/src/context/ProveedorSesion.jsx
@@ -1,11 +1,16 @@
 import { createContext } from "react"
 import { useNavigate } from "react-router-dom";
+import { supabase } from "../supabase/config";
 
 const ContextoSesion = createContext();
 
 const ProveedorSesion = ({children}) => {
 
     const navegar = useNavigate();
+
+    const registrarUsuario = () => {
+        supabase.auth.signUp({correo, password});
+    }
 
     const elementosExportados = { navegar };
     return(
--- Lista Compra Supabase/lista-compra-supabase/src/pages/InicioSesionRegistro.jsx
+++ Lista Compra Supabase/lista-compra-supabase/src/pages/InicioSesionRegistro.jsx
@@ -1,19 +1,68 @@
-const InicioSesionRegistro = () => {
-    return(
-        <>
-        <div id="cuerpoInicioRegistro">
-            <div id="marcoRegistro">
-                <form>
-                    <div>
-                    <label htmlFor="inputNombre">Nombre: </label>
-                    <input type="text" name="inputNombre" id="inputNombre" />
-                    </div>
-                    <label htmlFor="inputEmail">Correo: </label>
-                    <input type="email" name="inputEmail" id="inputEmail" />
+import { useState } from "react";
+import "./InicioSesionRegistro.css";
 
-                </form>
+const InicioSesionRegistro = () => {
+
+    const datosFormularioRegistro = {
+        "nombre": "",
+        "correo": "",
+        "password": ""
+    }
+
+    const [datosRegistro, setDatosRegistro] = useState(datosFormularioRegistro);
+
+    const actualizarDatoRegistro = (valor, nombre) => {
+        let datosRecogidos = datosRegistro;
+        datosRecogidos[nombre] = valor;
+        setDatosRegistro(datosRecogidos);
+    }
+
+    return (
+        <>
+            <div id="cuerpoInicioRegistro">
+                <div id="marcoRegistro">
+                    <h3>¿Todavía no te has registrado?</h3>
+                    <form>
+                        <div className="campoFormulario">
+                            <label htmlFor="nombre"><strong>Nombre: </strong></label>
+                            <input type="text" name="nombre" id="inputNombreRegistro" onChange={(evento) =>{
+                                actualizarDatoRegistro(evento.target.value, evento.target.name);
+                            }}/>
+                        </div>
+                        <div className="campoFormulario">
+                            <label htmlFor="correo">Correo: </label>
+                            <input type="email" name="correo" id="inputEmailRegistro" onChange={(evento) =>{
+                                actualizarDatoRegistro(evento.target.value, evento.target.name);
+                            }}/>
+                        </div>
+                        <div className="campoFormulario">
+                            <label htmlFor="password"><strong>Contraseña: </strong></label>
+                            <input type="password" name="password" id="inputPasswordRegistro" onChange={(evento) =>{
+                                actualizarDatoRegistro(evento.target.value, evento.target.name);
+                            }}/>
+                        </div>
+                        <div className="botonFormulario">
+                            <input type="button" value="Registrarme" />
+                        </div>
+                    </form>
+                </div>
+                <div id="marcoLogin">
+                    <h2>Accede a la App</h2>
+                    <form>
+                        <div className="campoFormulario">
+                            <label htmlFor="inputEmail"><strong>Correo: </strong></label>
+                            <input type="email" name="inputEmail" id="inputEmailLogin" />
+                        </div>
+                        <div className="campoFormulario">
+                            <label htmlFor="inputPassword"><strong>Contraseña: </strong></label>
+                            <input type="password" name="inputPassword" id="inputPasswordLogin" />
+                        </div>
+                        <div className="botonFormulario">
+                            <input type="button" value="Acceder" />
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
         </>
     )
 }
