a7a8146a6bc036090751e38686484f2f23149732
Alejandro <alexsc199x@gmail.com>
2026-02-10 16:46:48 +0100

    empezando última práctica DWEC

---
+++ Lista Compra Supabase/lista-compra-supabase/src/pages/AdministrarRoles.jsx
@@ -0,0 +1,16 @@
+import "./AdministrarRoles.css";
+
+const AdministrarRoles = () => {
+  return (
+    <>
+        <div id="cuerpoAdministrarRoles">
+            <h3>Cambiar rol de los usuarios</h3>
+            <form>
+                
+            </form>
+        </div>
+    </>
+  )
+}
+
+export default AdministrarRoles
